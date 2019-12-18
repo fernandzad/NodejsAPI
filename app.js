d7f14e06f2a068528fae2c097e0198b39294bed2
Adrián <ing.adrian94fdz@gmail.com>
2019-12-17 18:08:21 -0600

    Link changed, guideline

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const MongoClient = require('mongodb').MongoClient;
 const app = express();
 
-//https://medium.com/@uix/building-a-rest-api-using-node-and-mongodb-bf7ac086e8f7
+//https://dev.to/lenmorld/rest-api-with-mongodb-atlas-cloud-node-and-express-in-10-minutes-2ii1
 
 //mongoDB connection string
 const uri = "mongodb+srv://ing.adrian94fdz@gmail.com:<password>@testcluster-6itcv.mongodb.net/test?retryWrites=true&w=majority";
@@ -13,6 +13,10 @@ app.use('/', routes);
 const client = new MongoClient(uri, { useUnifiedTopology: true, useNewUrlParser: true});
 client.connect(err => {
     //const collection = client.db("test").collection("devices");
+    if(err){
+        console.log('Error ocurred while connecting to MongoDB Atlas', err);
+    }
+
     app.listen(3000);
     console.log('Base de Datos conectada!');
     client.close();
